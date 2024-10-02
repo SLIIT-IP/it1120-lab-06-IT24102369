b50830953a54541d9e89e473e4bbad5b555e7b2a
IT24102369 <IT24102369@MY.SLIIT.LK>
2024-10-02 15:21:52 +0530

    Add files via upload

---
+++ Q1/IT24102369Lab6Q1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IT24102369Lab6Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+        
+        double square = Math.pow(number, 2);
+        double squareRoot = Math.sqrt(number);
+        
+        System.out.println("Square of " + number + " is: " + square);
+        System.out.println("Square root of " + number + " is: " + squareRoot);
+    }
+}
